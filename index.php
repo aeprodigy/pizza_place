5802c6dc2c125c3b3ac3471ee4fe5bb78e5a1ea9
aeprodigy <mikeapple056@gmail.com>
2024-01-01 10:21:18 +0200

    Header

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>The Pizza Place</title>
 </head>
 <body>
-    
+    <h1>The Pizza Place</h1>
 </body>
 </html>
