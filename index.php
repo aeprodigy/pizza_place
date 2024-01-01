8c51e421de96670f4f29d8a0473fba967bb8148c
aeprodigy <mikeapple056@gmail.com>
2024-01-01 17:42:07 +0200

    the folder structure

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Pizza Place</title>
+    <link rel="stylesheet" href="./styles/styles.css">
 </head>
 <body>
     <h1>The Pizza Place</h1>
